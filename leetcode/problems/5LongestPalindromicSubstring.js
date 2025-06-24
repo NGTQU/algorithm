956e9ab0def8a189d0ad4d42071d0099757d6631
Quyen Nguyen <ngtqu000@gmail.com>
2025-06-24 15:31:13 +0700

    TEST999999 - Test

--- leetcode/problems/5LongestPalindromicSubstring.js
+++ leetcode/problems/5LongestPalindromicSubstring.js
@@ -4,18 +4,18 @@
  */
 
 const longestPalindrome = (s) => {
-    let res = '';
-    let substring = '';
+  let res = "";
+  let substring = "";
 
-    for (let j = 0; j < s.length; j++) {
-        for (let k = j; k < s.length; k++) {
-            substring = s.slice(j, k);
-            if (isPalindrome(substring) && res.length < substring.length) {
-                res = substring;
-            }
-        }
+  for (let j = 0; j < s.length; j++) {
+    for (let k = j; k < s.length; k++) {
+      substring = s.slice(j, k);
+      if (isPalindrome(substring) && res.length < substring.length) {
+        res = substring;
+      }
     }
-    return res;
+  }
+  return res;
 };
 
 /**
@@ -23,19 +23,20 @@ const longestPalindrome = (s) => {
  * @return {boolean}
  */
 const isPalindrome = (s) => {
-    const lastIndex = s.length - 1;
-    const middleIndex = Math.round(s.length/2) - 1;
-    for (let i = 0; i <= middleIndex; i++) {
-        if (s[i] === s[lastIndex-i]) {
-            if (i === middleIndex) {
-                return true;
-            }
-        } else {
-            return false;
-        }
+  const lastIndex = s.length - 1;
+  const middleIndex = Math.round(s.length / 2) - 1;
+  for (let i = 0; i <= middleIndex; i++) {
+    if (s[i] === s[lastIndex - i]) {
+      if (i === middleIndex) {
+        return true;
+      }
+    } else {
+      return false;
     }
-}
+  }
+};
 
-console.log(longestPalindrome('babad'));
-console.log(longestPalindrome('cbbd'));
-console.log(longestPalindrome('a'));
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));
+console.log(longestPalindrome("a"));
+console.log("abc");
