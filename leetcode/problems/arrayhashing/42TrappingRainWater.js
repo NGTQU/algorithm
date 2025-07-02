7deca8e16990765c3b65fab62645cca511554fa0
Quyen Nguyen <ngtqu000@gmail.com>
2025-07-03 00:31:26 +0700

    EX#42 - Solve Trapping Rain Water

---
+++ leetcode/problems/arrayhashing/42TrappingRainWater.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+const trap = (heights) => {
+  let trappedArea = 0;
+
+  return trappedArea;
+};
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log(trap([4, 2, 0, 3, 2, 5]));
+console.log(trap([5, 2, 1, 2, 1, 5]));
+console.log(trap([0, 2, 0]));
+console.log(trap([4, 2, 3]));
