6a67c11c825219ade368369bb0d07fe1efcf259d
Quyen Nguyen <ngtqu000@gmail.com>
2025-11-04 20:44:24 +0700

    EX#121 - Solve Best Time To Buy And Sell Stock

---
+++ leetcode/problems/slidingwindow/121BestTimeToBuyAndSellStock.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices) => {};
+
+console.log(maxProfit([1, 2]));
+console.log(maxProfit([2, 1, 2, 1, 0, 1, 2]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([1, 4, 2]));
+console.log(maxProfit([2, 1, 2, 0, 1]));
